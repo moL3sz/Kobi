606da4f29b1b646ded8d4b626caa5ffe290f2397
moL3sz <molnarok2313@gmail.com>
2021-09-16 19:08:35 +0200

    keveropult

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        val keveropult = findViewById<Button>(R.id.asd)
+        keveropult.setOnClickListener{
+            Toast.makeText(this@MainActivity,"You clicked me.",Toast.LENGTH_SHORT).show();
+        }
+    }   
 }
